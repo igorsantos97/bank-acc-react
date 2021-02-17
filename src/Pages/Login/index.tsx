1fef7014e77753c14c7bce69fd473fb9e5f73068
Igor Santos <igornzds@gmail.com>
2021-02-17 17:54:48 -0300

    Primeiro commit

---
+++ src/Pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Login: React.FC = () => {
+  return (
+    <Title>oi</Title>
+  )
+}
+
+export default Login;
+
+
---
+++ src/routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Landing from './Pages/Landing';
+import Login from './Pages/Login';
+
+const Routes: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                {/* Rotas */}
+
+                <Route path="/" exact component={Landing} />
+                <Route path="/login" component={Login} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
