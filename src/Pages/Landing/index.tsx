cc15e9f7f1ee0ee6fcbdf90878d4c263078c6337
Igor Santos <igornzds@gmail.com>
2021-02-18 18:53:20 -0300

    create the homepage banner

--- src/Pages/Landing/index.tsx
+++ src/Pages/Landing/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import ButtonSumit from '../../components/ButtonSubmit';
+
 import Header from '../../components/Header';
 import Container from '../../components/MainContainer/styles';
-
-import { MainBanner, MainBannerContentLeft } from './styles';
+import TitleForm from '../../components/TitleForm';
+import { MainBanner, MainBannerContentLeft, MainBannerContent, MainBannerContentRight, Form, InputForm } from './styles';
 
 const Landing: React.FC = () => {
   return (
@@ -11,10 +15,28 @@ const Landing: React.FC = () => {
 
       <MainBanner>
         <Container>
-          <MainBannerContentLeft>
-            <p>Oi</p>
+          <MainBannerContent>
 
-          </MainBannerContentLeft>
+            <MainBannerContentLeft>
+              <p>Gama Bank é um projeto de nossos alunos. <span>Já tem conta?</span></p>
+              <Link to="/login">
+                <ButtonSumit>Acessar <FaArrowRight /></ButtonSumit>
+              </Link>
+            </MainBannerContentLeft>
+
+            <MainBannerContentRight>
+              <Form>
+                <TitleForm>Peça sua conta e cartão de crédito do Gama Bank</TitleForm>
+                <InputForm placeholder="Digita seu CPF" />
+                <InputForm placeholder="Escolha um nome de usuário" />
+                <InputForm placeholder="Nome completo" />
+                <InputForm placeholder="Digite sua senha" />
+                <InputForm placeholder="Confirme sua senha" />
+                <ButtonSumit>Continuar <FaArrowRight /></ButtonSumit>
+              </Form>
+            </MainBannerContentRight>
+
+          </MainBannerContent>
         </Container>
       </MainBanner>
     </>
--- src/Pages/Landing/styles.ts
+++ src/Pages/Landing/styles.ts
@@ -4,12 +4,87 @@ import ImgBg from '../../assets/background-landing-1.png';
 
 export const MainBanner = styled.section`
   background: url(${ImgBg}) no-repeat;
-  background-size: 40%;
-  background-position: center bottom;
+  background-size: auto;
+  background-position: 42% bottom;
   width: 100%;
   min-height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const MainBannerContent = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
 `;
 
 export const MainBannerContentLeft = styled.div`
+  height: auto;
+  max-width: 280px;
+  color: #fff;
+  font-size: 30px;
+
+  span {
+    display: block;
+    margin: 40px 0 30px;
+  }
+
+  @media (max-width: 676px) {
+    margin-top: 160px;
+    margin-bottom: 22px;
+    text-align: center;
+    max-width: 70%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
+
+export const InputForm = styled.input`
+  margin-top: 50px;
+  border: none;
+  border-bottom: 1px solid #878686;
+  width: 100%;
+  padding: 10px 14px;
+  font-size: 14px;
+
+  &::placeholder {
+    color: #878686;
+  }
+
+  &:focus {
+    border-bottom: 1px solid #68de5a;
+  }
+
+  & + input {
+    margin-top: 30px;
+  }
+`;
+
+export const Form = styled.form`
+  padding: 40px 46px;
   background: #fff;
+  min-height: 450px;
+  border-radius: 8px;
+  width: 350px;
+
+  button {
+    background: #d8d8d8;
+    color: #9b9b9b;
+    margin-top: 30px;
+  }
+`;
+
+export const MainBannerContentRight = styled.div`
+  @media (max-width: 676px) {
+    margin: 0 auto;
+    max-width: 100%;
+  }
+
+  form {
+    @media (max-width: 676px) {
+      width: 100%;
+    }
+  }
 `;
