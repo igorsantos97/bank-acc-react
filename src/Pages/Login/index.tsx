e6629c1b76d8fe80475d9652a81eb8ca948994de
Igor Santos <igornzds@gmail.com>
2021-02-18 19:15:12 -0300

    create login page

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Header from '../../components/Header';
+import InputForm from '../../components/InputForm';
 
-import { Title } from './styles';
+import { Form, FormTitle, FormButtonSubmit } from './styles';
 
 const Login: React.FC = () => {
   return (
-    <Title>oi</Title>
+    <>
+      <Header />
+      <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center' }}>
+        <Form>
+          <FormTitle>Faça o login</FormTitle>
+
+          <InputForm name="user" placeholder="Digite seu usuário" />
+          <InputForm name="password" placeholder="Digite sua senha" type="password" />
+
+          <FormButtonSubmit>
+            Continuar<FaArrowRight />
+          </FormButtonSubmit>
+
+          <Link to="#">Esqueci minha senha</Link>
+          <Link to="#">Ainda não sou cliente</Link>
+
+        </Form>
+      </div>
+    </>
   )
 }
 
