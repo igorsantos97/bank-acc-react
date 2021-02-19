7538adff4327b6fd559fe0befd32473679471d4f
Igor Santos <igornzds@gmail.com>
2021-02-19 15:57:39 -0300

    update application routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Error from './Pages/Error';
 import Landing from './Pages/Landing';
 import Login from './Pages/Login';
+import Recover from './Pages/Recover';
 
 const Routes: React.FC = () => {
     return (
@@ -12,6 +14,8 @@ const Routes: React.FC = () => {
 
                 <Route path="/" exact component={Landing} />
                 <Route path="/login" component={Login} />
+                <Route path="/recover" component={Recover} />
+                <Route path="/error" component={Error} />
             </Switch>
         </BrowserRouter>
     );
